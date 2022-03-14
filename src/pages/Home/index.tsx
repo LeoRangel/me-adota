8d3b4ebce3160eec1d03b7b73864d7c0cb94be27
LeoRangel <leandro.mdrs06@gmail.com>
2022-03-14 20:05:00 -0300

    Add React Router Dom and config Public Router

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+import { Wrapper } from "../../components"
+
+export function Home() {
+    return (
+        <S.MainContent>
+            <h1>Home page</h1>
+        </S.MainContent>
+    );
+}
