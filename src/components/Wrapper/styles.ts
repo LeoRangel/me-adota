933e679d3b07bdd3b4d1226940bb508120610bb3
LeoRangel <leandro.mdrs06@gmail.com>
2022-03-16 20:00:30 -0300

    Add Wrapper component

---
+++ src/components/Wrapper/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding: var(--pd-container);
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
+import { Wrapper } from '../../components';
 import * as S from './styles';
 
 export function Home() {
   return (
     <S.MainContent>
-      <h1>Home page</h1>
+      <Wrapper>
+        <h1>Home page</h1>
+      </Wrapper>
     </S.MainContent>
   );
 }
