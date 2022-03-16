541b03a4282bd5d513c045726594c9f264137df6
LeoRangel <leandro.mdrs06@gmail.com>
2022-03-16 19:49:08 -0300

    Project config: eslint, prettier and editorconfig

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import * as S from './styles';
-import { Wrapper } from "../../components"
 
 export function Home() {
-    return (
-        <S.MainContent>
-            <h1>Home page</h1>
-        </S.MainContent>
-    );
+  return (
+    <S.MainContent>
+      <h1>Home page</h1>
+    </S.MainContent>
+  );
 }
